dd27958735ab91a80433a24faaecf1e80b082f76
Fabian Schmengler <fs@integer-net.de>
2020-09-02 18:03:51 +0200

    Add example tests

---
+++ tests/integration/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace :module-namespace\:module-name;
+
+use PHPUnit\Framework\TestCase;
+use Magento\TestFramework\Helper\Bootstrap;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function we_can_use_the_object_manager()
+    {
+        $this->assertInstanceOf(Magento\TestFramework\ObjectManager::class, Bootstrap::getObjectManager());
+    }
+}
---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace :module-namespace\:module-name;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
